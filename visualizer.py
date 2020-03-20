b5e583fe9ca4ede809711ff80af8b8509c12c4d4
gary-269 <karanveersingh269@gmail.com>
2020-03-19 20:04:32 -0400

    Visualised data using pandas

---
+++ visualizer.py
@@ -0,0 +1,36 @@
+from os import chdir
+from glob import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+
+
+def merge(indir="/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Analysed Data",
+          out_file="Merged.csv"):
+    chdir(indir)
+    file_list = glob("*.csv")
+    df_list = []
+    for filename in file_list:
+        print(filename)
+        df = pd.read_csv(filename)
+        df_list.append(df)
+    concatenated_df = pd.concat(df_list, axis=0)
+    concatenated_df.to_csv(out_file, index=None)
+
+
+def visualise():
+    data = pd.read_csv(
+        "/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Analysed Data/Merged.csv",
+        usecols=['Polarity'])
+    # print(data)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    data.hist(bins=[-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1],
+              ax=ax,
+              color="red")
+
+    # plt.title("Sentiments from Tweets on CAA")
+    plt.show()
+
+
+if __name__ == '__main__':
+    visualise()
