2246ab2d437c1d88bef062e105283031da16db7c
gary-269 <karanveersingh269@gmail.com>
2020-03-24 15:39:13 -0400

    Create mapper.py

---
+++ mapper.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from geopandas.tools import geocode
+
+data = pd.read_csv(
+    "/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Analysed Data/Merged.csv")
