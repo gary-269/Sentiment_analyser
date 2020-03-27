d8a9445825c7abd90cbc58e484aa373bbb7ebdd4
gary-269 <karanveersingh269@gmail.com>
2020-03-27 17:49:09 -0400

    Updated mapper

--- mapper.py
+++ mapper.py
@@ -1,5 +1,18 @@
 import pandas as pd
 from geopandas.tools import geocode
+import geopy
 
-data = pd.read_csv(
-    "/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Analysed Data/Merged.csv")
+data = pd.read_csv("/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Dataset/Day1.csv")
+# pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+
+for index, row in data.iterrows():
+    try:
+        print(row['Location'])
+        info = geocode(str(row['Location']), provider='Nominatim')
+        data.loc[int(index), 'Address'] = info['address'].loc[0]
+        data.loc[int(index), 'Longitude'] = info['geometry'].loc[0].x
+        data.loc[int(index), 'Latitude'] = info['geometry'].loc[0].y
+    except BaseException:
+        pass
+
