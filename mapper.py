3a7e0fd1e2cda29b918a9a83fe99d1d295a5dae6
gary-269 <karanveersingh269@gmail.com>
2020-03-28 22:12:23 -0400

    Extracted coordinates of locations

--- mapper.py
+++ mapper.py
@@ -2,17 +2,17 @@ import pandas as pd
 from geopandas.tools import geocode
 import geopy
 
-data = pd.read_csv("/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Dataset/Day1.csv")
-# pd.set_option("display.max_rows", None, "display.max_columns", None)
-
+data = pd.read_csv("/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/Dataset/Day4.csv")
 
 for index, row in data.iterrows():
     try:
-        print(row['Location'])
+        # print(row['Location'])
         info = geocode(str(row['Location']), provider='Nominatim')
         data.loc[int(index), 'Address'] = info['address'].loc[0]
         data.loc[int(index), 'Longitude'] = info['geometry'].loc[0].x
         data.loc[int(index), 'Latitude'] = info['geometry'].loc[0].y
     except BaseException:
         pass
-
+data = data.dropna()
+# print(data)
+data.to_csv("/Users/karanveersingh/PycharmProjects/Sentiment_analyser/Sentiment_analyser/LocationInfo/loc4.csv")
