85502fddfb45fed575087e90f7eeac6bb8c497be
Karanveer Singh <43607060+gary-269@users.noreply.github.com>
2020-04-04 01:07:13 -0400

    Delete twitter_credentials.py

--- Code/twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-#Variables containing data for api
-api_key = "cv1kS1qvj3yqxzWOh6oeuCSBh"
-api_secret_key = "NXCBfpaUd5UH7MNX1AcGVne6Evtd9Ze0biADZvNyJJ3fNW9wLJ"
-access_token = "1221189112742469637-zqhYjmykZMBcTrzbUSV1Rw0yMX30Wg"
-access_token_secret = "ayoiklzCkjhNbngpTjMixmmoJVHEe8qojIgBT9mJX3o31"
